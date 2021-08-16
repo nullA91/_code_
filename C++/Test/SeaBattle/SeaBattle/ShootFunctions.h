2fefbadf3e4f74b2efb53d2a944e9da74205c2c5
nullA91 <test4git@mail.ru>
2021-08-16 16:41:15 +0300

    Delete C++/Test directory

--- C++/Test/SeaBattle/SeaBattle/ShootFunctions.h
+++
@@ -1,5 +0,0 @@
-#pragma once
-
-#include "GLOBAL.h"
-
-void shoot(char array[size * 3][size * 5], int Y, int X);
--- C++/Test/test/test/GLOBAL.h
+++
@@ -1,7 +0,0 @@
-#pragma once
-
-#include <iostream>
-
-static int hit = 4;
-
-void func();
