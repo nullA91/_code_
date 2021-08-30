e4029dddc3a82d5adba4020d46d02008848529a1
nullA91 <test4git@mail.ru>
2021-08-30 15:57:21 +0300

    update GLOBAL.h

--- C++/ItStep/Other/SeaBattle/SeaBattle/GLOBAL.h
+++ C++/ItStep/Other/SeaBattle/SeaBattle/GLOBAL.h
@@ -3,24 +3,19 @@
 #include <cstdlib>
 #include <ctime>
 #include <iostream>
-
-const char symbol = static_cast<char>(254);
-
-const int row = 30;
-const int column = 50;
-const int size = 10;
-
-const int r_step = 3;
-const int c_step = 5;
-
-const int rows[size] = { 1, 4, 7, 10, 13, 16, 19, 22, 25, 28 };
-const int columns[size] = { 2, 7, 12, 17, 22, 27, 32, 37, 42, 47 };
-
-// const char b_letters[10]{ 'A','B','C','D','E','F','G','H','I','J' };
-// const char s_letters[10]{ 'a','b','c','d','e','f','g','h','i','j' };
-
-void _function_output_filed(char array[row][column], const int size);
+#include <Windows.h>
 
 static int attempt = 0;
 static int part = 1;
-static int hit = 0;
+
+const char symbol = static_cast<char>(254);
+
+const int rows = 30;
+const int columns = 50;
+
+const int _step_row = 3;
+const int _step_column = 5;
+
+void _function_output_filed(const char field[rows][columns]);
+void _function_console_cursor(int x, int y);
+int length(int array[]);
